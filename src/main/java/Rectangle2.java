d80b6e70e122bda010271c7b3cb878794298d714
Andrew Telleria and Chris Houghton <atelleria08@gmail.com>
2016-02-15 09:47:11 -0800

    Intial files

---
+++ src/main/java/Rectangle2.java
@@ -0,0 +1,23 @@
+public class Rectangle2 {
+private int mLength;
+private int mWidth;
+
+
+public Rectangle2(int length, int width) {
+  mLength = length;
+  mWidth = width;
+
+ }
+
+ public int getLength() {
+   return mLength;
+ }
+
+ public int getWidth() {
+   return mWidth;
+ }
+
+ public boolean isSquare() {
+   return mLength == mWidth;
+ }
+}
---
+++ src/test/java/Rectangle2Test.java
@@ -0,0 +1,34 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class Rectangle2Test {
+
+  @Test
+  public void newRectangle2_instantiatesCorrectly() {
+    Rectangle2 testRectangle2 = new Rectangle2(2,2);
+    assertEquals(true, testRectangle2 instanceof Rectangle2);
+  }
+
+  @Test
+  public void newRectangle2_setsLength() {
+    Rectangle2 testRectangle2 = new Rectangle2(2, 2);
+    assertEquals(2, testRectangle2.getLength());
+  }
+
+  @Test
+  public void newRectangle2_setWidth() {
+    Rectangle2 testRectangle2 = new Rectangle2(2, 2);
+    assertEquals(2, testRectangle2.getWidth());
+  }
+
+  @Test
+  public void isSquare_whenNotASquare_false() {
+    Rectangle2 testRectangle2 = new Rectangle2(2, 4);
+    assertEquals(false, testRectangle2.isSquare());
+  }
+
+  @Test public void isSquare_allSidesEqual_true() {
+    Rectangle2 testRectangle2 = new Rectangle2(2, 2);
+    assertEquals(true, testRectangle2.isSquare());
+  }
+}
